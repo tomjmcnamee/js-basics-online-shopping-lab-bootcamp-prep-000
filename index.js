46b66a4b604cc69a7e6a9069b002eb777841c676
Tom McNamee <tom@tomjmcnamee.com>
2019-03-13 02:39:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -48,5 +48,5 @@ function removeFromCart(item) {
 function placeOrder(cardNumber) {
   if ( cardNumber === undefined ) { return "Sorry, we don't have a credit card on file for you."}
   else {  return `Your total cost is \$${total()}, which will be charged to the card ${cardNumber}.`
-    cart = [] }
+    cart.length = 0 }
   }
